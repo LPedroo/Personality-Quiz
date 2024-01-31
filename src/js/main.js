d6cd33228d2714e2e345907536002a964765844b
LPedroo <plazabr3@gmail.com>
2024-01-31 11:06:38 -0300

    Atualização - Preparando a lógica

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const questions = [
   {
     question:
       "1) Qual é a coisa mais importante para você em um relacionamento?",
+
     choices: [
       "a) Conexão emocional profunda",
 
@@ -11,25 +12,31 @@ const questions = [
 
       "d) Visão de longo prazo e metas compartilhadas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "2) Como você lida com o estresse?",
+
     choices: [
       "a) Procurando apoio emocional",
 
-      "b) Buscando soluções inovadoras",
+      "b) Buscando distrações ",
 
-      "c) Enfrentando desafios com determinação",
+      "c) Enfrentando o problema",
 
-      "d) Mantendo o foco em objetivos futuros",
+      "d) Mantendo o foco no que é importante pra mim",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "3) Qual atividade você prefere em seu tempo livre?",
+
     choices: [
-      "a) Envolver-se em conversas profundas",
+      "a)  Conversar ou conhecer pessoas ",
 
       "b) Explorar novas ideias e projetos",
 
@@ -37,10 +44,13 @@ const questions = [
 
       "d) Planejar o futuro e definir metas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "4) Como você reage a mudanças inesperadas?",
+
     choices: [
       "a) Busco compreender as emoções envolvidas",
 
@@ -50,114 +60,141 @@ const questions = [
 
       "d) Avalio como isso impactará meus objetivos futuros",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "5)Qual é o seu maior medo?",
-    choices: [
-      "a) Perder conexões emocionais importantes",
 
-      "b) Ficar preso na mesmice sem inovação",
+    choices: [
+      "a) Perder pessoas importantes da minha vida ",
+
+      "b) Ficar preso na mesmice",
 
       "c) Não conseguir superar desafios",
 
-      "d) Não atingir as metas de longo prazo",
+      "d) Não atingir meus objetivos de vida",
     ],
-    answer: 1,
-  },
-  {
-    question: "6)O que mais motiva suas decisões?",
-    choices: [
-      "a) Emoções e relações interpessoais",
 
-      "b) Oportunidades para inovar",
+    answer: [0, 1, 2, 3],
+  },
+
+  {
+    question: "6) O que mais motiva suas decisões?",
+
+    choices: [
+      "a) Emoções e relações",
+
+      "b) Oportunidades",
 
       "c) Vontade de superar obstáculos",
 
-      "d) Alcançar metas de longo prazo",
+      "d) Alcançar metas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "7) Como você prefere resolver conflitos?",
-    choices: [
-      "a) Priorizando a compreensão emocional",
 
-      "b) Propondo soluções criativas",
+    choices: [
+      "a) Priorizando a compreensão",
+
+      "b) Propondo soluções",
 
       "c) Lidando com resiliência e paciência",
 
-      "d) Considerando o impacto a longo prazo",
+      "d) Considerando como isso pode me afetar",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "8) O que te faz sentir mais realizado?",
+
     choices: [
       "a) Relações interpessoais significativas",
 
-      "b) Contribuições inovadoras",
+      "b) Contribuições",
 
       "c) Superar desafios pessoais",
 
-      "d) Alcançar metas de longo prazo",
+      "d) Concluir objetivos",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: " 9) Qual é a sua abordagem para enfrentar problemas?",
-    choices: [
-      "a) Buscar apoio emocional",
 
-      "b) Encontrar soluções inovadoras",
+    choices: [
+      "a) Buscar apoio",
+
+      "b) Encontrar soluções rapidamente",
 
       "c) Encarar com resiliência",
 
-      "d) Planejar estrategicamente a longo prazo",
+      "d) Planejar estrategicamente",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "10) O que você valoriza mais em um projeto?",
+
     choices: [
-      "a) A conexão emocional que ele cria",
+      "a) A conexão que ele cria com as pessoas",
 
       "b) A inovação e originalidade",
 
       "c) A capacidade de superar desafios",
 
-      "d) A contribuição para objetivos futuros",
+      "d) A contribuição para objetivos",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "11) Como você se sente em relação a grandes mudanças na vida?",
+
     choices: [
       "a) Emocionalmente conectado(a)",
 
-      "b) Animado/a com novas possibilidades",
+      "b) Animado(a) com novas possibilidades",
 
-      "c) Determinado/a a enfrentar desafios",
+      "c) Determinado(a) a enfrentar",
 
-      "d) Focado/a em objetivos de longo prazo",
+      "d) Focado(a) na minha vida ",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "12) O que mais te atrai em uma história ou filme?",
+
     choices: [
-      "a) Relacionamentos emocionais complexos",
+      "a) Relacionamentos emocionais",
 
-      "b) Elementos inovadores e surpreendentes",
+      "b) Roteiros surpreendentes",
 
-      "c) Personagens resilientes em situações difíceis",
+      "c) Personagens em situações difíceis",
 
-      "d) Narrativas que exploram o futuro e metas",
+      "d) Narrativas objetivas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "13) Qual característica você mais valoriza em um amigo?",
+
     choices: [
       "a) Empatia e compreensão",
 
@@ -165,116 +202,143 @@ const questions = [
 
       "c) Resiliência e coragem",
 
-      "d) Compartilhamento de metas de longo prazo",
+      "d) Compartilhamento de experiências e objetivos de vida",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "14) Como você prefere aprender algo novo?",
+
     choices: [
       "a) Com experiências emocionais e relacionais",
 
-      "b) Buscando abordagens inovadoras",
+      "b) Buscando abordagens diferentes ",
 
       "c) Superando desafios práticos",
 
       "d) Com um foco a longo prazo",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
-    question: "15) O que te faz sentir mais conectado/a ao mundo?",
+    question: "15) O que te faz sentir mais conectado(a) ao mundo?",
+
     choices: [
-      "a) Relações interpessoais significativas",
+      "a) Relações",
 
       "b) Descobrir novas ideias e conceitos",
 
       "c) Superar desafios pessoais",
 
-      "d) Contribuir para metas de longo prazo",
+      "d) Contribuir positivamente ",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "16) Como você define sucesso pessoal?",
+
     choices: [
       "a) Ter relacionamentos felizes e saudáveis",
 
-      "b) Alcançar objetivos inovadores",
+      "b) Alcançar objetivos",
 
       "c) Superar desafios e adversidades",
 
-      "d) Cumprir metas de longo prazo",
+      "d) Cumprir metas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "17) O que mais te inspira em uma obra de arte?",
+
     choices: [
-      "a) Expressões emocionais profundas",
+      "a) Expressões emocionais",
 
       "b) Elementos criativos e originais",
 
-      "c) Mensagens de resiliência e superação",
+      "c) Mensagens",
 
-      "d) Representações de visões de futuro",
+      "d) Representações",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "18) O que você busca em uma carreira profissional?",
+
     choices: [
-      "a) Ambiente de trabalho com relações sólidas",
+      "a) Bom ambiente de trabalho",
 
-      "b) Oportunidades para inovação constante",
+      "b) Oportunidades constante",
 
-      "c) Desafios que exigem resiliência",
+      "c) Desafios",
 
-      "d) Contribuição para metas a longo prazo",
+      "d) Contribuição",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "19) Como você prefere passar suas férias?",
+
     choices: [
       "a) Com amigos e familiares próximos",
 
       "b) Explorando novas ideias e lugares",
 
-      "c) Envolvido/a em atividades desafiadoras",
+      "c) Envolvido(a) em atividades",
 
       "d) Planejando para o futuro",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "20) O que você mais valoriza em uma equipe de trabalho?",
+
     choices: [
       "a) A coesão e empatia entre os membros",
 
-      "b) A capacidade de inovação e criatividade",
+      "b) A capacidade de criatividade",
 
-      "c) A resiliência diante de desafios",
+      "c) Enfrentar os desafios",
 
-      "d) A visão compartilhada de metas a longo prazo",
+      "d) A visão compartilhada",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
-    question: "21) Como você lida com feedback construtivo?",
+    question: "21) Como você lida com feedback negativo ?",
+
     choices: [
-      "a) Valorizo a compreensão emocional envolvida",
+      "a) Busco a compreensão",
 
-      "b) Vejo como uma oportunidade para inovar",
+      "b) Vejo como uma oportunidade para melhorar",
 
-      "c) Aceito e busco melhorar com resiliência",
+      "c) Aceito sem questionar",
 
-      "d) Considero no contexto de objetivos futuros",
+      "d) Se eu achar injusto eu questiono",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "22) O que mais te atrai em um hobby?",
+
     choices: [
       "a) O aspecto social e emocional",
 
@@ -284,75 +348,93 @@ const questions = [
 
       "d) A conexão com objetivos de longo prazo",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "23) Como você aborda a resolução de problemas cotidianos?",
+
     choices: [
       "a) Com base na compreensão emocional",
 
-      "b) Buscando soluções inovadoras",
+      "b) Buscando soluções diferentes ",
 
-      "c) Lidando com resiliência e paciência",
+      "c) Lidando com paciência",
 
-      "d) Considerando o impacto a longo prazo",
+      "d) Considerando o impacto negativo",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "24) O que mais te intriga em uma história de vida?",
+
     choices: [
-      "a) Relacionamentos interpessoais significativos",
+      "a) Como as pessoas se conectam",
 
       "b) Momentos inovadores e decisivos",
 
-      "c) A resiliência diante de desafios",
+      "c) A vontade de vencer desafios",
 
-      "d) A jornada em direção a metas de longo prazo",
+      "d) A jornada em direção a metas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "25) Como você se sente em relação a regras e estruturas?",
+
     choices: [
-      "a) Valorizo a flexibilidade e as conexões emocionais",
+      "a) Valorizo a flexibilidade",
 
-      "b) Vejo como oportunidades para inovar",
+      "b) Vejo como oportunidades para me aproximar",
 
-      "c) Aceito e adapto-me com resiliência",
+      "c) Aceito e adapto-me",
 
       "d) Considero em relação aos objetivos futuros",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "26) O que mais te fascina em uma conversa?",
+
     choices: [
       "a) A profundidade das emoções compartilhadas",
 
-      "b) As ideias inovadoras e surpreendentes",
+      "b) As ideias propostas",
 
-      "c) A resiliência expressa diante de desafios",
+      "c) A vivência que foi expressada",
 
-      "d) A discussão de metas e planos futuros",
+      "d) Conversas sobre metas e sonhos",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "27) Como você escolhe seu próximo desafio?",
+
     choices: [
-      "a) Considerando o impacto emocional",
+      "a) Considerando o impacto no meu emocional ",
 
       "b) Buscando algo inovador e único",
 
-      "c) Optando por algo que exija resiliência",
+      "c) Optando por algo que exija força de vontade ",
 
-      "d) Avaliando em relação a metas de longo prazo",
+      "d) Que sejam benéficas ao meu futuro",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "28) O que mais te atrai em um livro?",
+
     choices: [
       "a) A profundidade dos personagens e relações",
 
@@ -360,61 +442,74 @@ const questions = [
 
       "c) A superação de obstáculos pelos protagonistas",
 
-      "d) A exploração de visões de futuro",
+      "d) A exploração da história",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "29)Como você se sente em relação a mudanças de planos?",
+
     choices: [
-      "a) Valorizo a adaptabilidade emocional",
+      "a) Valorizo a adaptabilidade",
 
-      "b) Vejo como oportunidade para inovação",
+      "b) Vejo como oportunidade",
 
-      "c) Aceito e enfrento com resiliência",
+      "c) Aceito e enfrento",
 
-      "d) Considero em relação aos objetivos futuros",
+      "d) Adapto as minhas metas",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "30) O que te faz sentir mais realizado(a) no trabalho?",
+
     choices: [
       "a) Relações interpessoais significativas",
 
-      "b) Contribuições inovadoras para a empresa",
+      "b) Contribuições para a empresa",
 
       "c) Superar desafios profissionais",
 
       "d) Alcançar metas de longo prazo",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "31) Qual é a sua reação diante de conquistas pessoais?",
+
     choices: [
       "a) Comemoro junto com as pessoas próximas",
 
-      "b) Vejo como uma oportunidade para inovação contínua",
+      "b) Vejo como uma inovação contínua",
 
       "c) Sinto uma sensação de resiliência e superação",
 
-      "d) Reflito sobre como isso contribui para meus objetivos futuros",
+      "d) Reflito sobre como isso contribui para o meu futuro",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
+
   {
     question: "32)O que mais te motiva a aprender algo novo?",
-    choices: [
-      "a) A oportunidade de compartilhar a experiência emocional",
 
-      "b) A busca por novas e inovadoras perspectivas",
+    choices: [
+      "a) A oportunidade de compartilhar a experiência",
+
+      "b) A busca por novas perspectivas",
 
       "c) O desafio de superar obstáculos práticos",
 
-      "d) A contribuição para metas e planos de longo prazo",
+      "d) A contribuição para planos futuros",
     ],
-    answer: 1,
+
+    answer: [0, 1, 2, 3],
   },
 ];
 
@@ -435,13 +530,56 @@ function showQuestion() {
     choicesElement.appendChild(choiceElement);
   });
 }
+// const userChoices = []; // Array para armazenar as escolhas do usuário
+
+// function checkAnswer(e) {
+//   const selectedChoice = questions[currentQuestion].choices[e.target.value];
+//   const correctAnswer = questions[currentQuestion].answer[currentQuestion];
+
+//   // Armazena a escolha do usuário
+//   userChoices.push(selectedChoice);
+
+//   currentQuestion++;
+
+//   if (currentQuestion < questions.length) {
+//     showQuestion();
+//   } else {
+//     // Agora, você pode usar o array userChoices para construir a lógica de personalidade
+//     displayPersonality();
+//   }
+// }
+
+// function displayPersonality() {
+//   // Aqui você pode usar o array userChoices para determinar a personalidade com base nas escolhas
+//   console.log("Escolhas do usuário:", userChoices);
+
+//   // Lógica de personalidade: exemplo simples
+//   const personalityTraits = userChoices.join(", ");
+//   alert("Sua personalidade: " + personalityTraits);
+// }
+
+var empathetic = 0;
+var innovative = 0;
+var resilient = 0;
+var visionary = 0;
 
 function checkAnswer(e) {
-  if (parseInt(e.target.value) === questions[currentQuestion].answer) {
-    alert("Correct!");
-  } else {
-    alert("Incorrect.");
+  if (parseInt(e.target.value) === questions[currentQuestion].answer[0]) {
+    empathetic++;
   }
+
+  if (parseInt(e.target.value) === questions[currentQuestion].answer[1]) {
+    innovative++;
+  }
+
+  if (parseInt(e.target.value) === questions[currentQuestion].answer[2]) {
+    resilient++;
+  }
+
+  if (parseInt(e.target.value) === questions[currentQuestion].answer[3]) {
+    visionary++;
+  }
+  console.log(empathetic, innovative, resilient, visionary);
 
   currentQuestion++;
 
@@ -450,6 +588,26 @@ function checkAnswer(e) {
   } else {
     alert("Quiz completed!");
   }
+
+  const pontuacoes = {
+    empatico: empathetic,
+    inovador: innovative,
+    resiliente: resilient,
+    visionario: visionary,
+  };
+
+  let personalidadeDominante;
+
+  for (const [personalidade, pontuacao] of Object.entries(pontuacoes)) {
+    if (
+      pontuacao > pontuacoes[personalidadeDominante] ||
+      personalidadeDominante === undefined
+    ) {
+      personalidadeDominante = personalidade;
+    }
+  }
+
+  console.log(`Você é ${personalidadeDominante}`);
 }
 
 showQuestion();
